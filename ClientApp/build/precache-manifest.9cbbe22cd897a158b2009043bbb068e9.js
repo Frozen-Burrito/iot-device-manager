cc0ad759e8d3ac384243b605a4fdda3845d1abf9
FrozenBurrito <f3rm3ndouza@outlook.com>
2021-09-27 18:02:27 -0500

    Client App Hotfix

---
+++ ClientApp/build/precache-manifest.9cbbe22cd897a158b2009043bbb068e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d7954a96b39dfca49d75bfb819a8b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2658f45c3018fbb7513",
+    "url": "/static/css/2.d770e966.chunk.css"
+  },
+  {
+    "revision": "b59225e8bfbff6ec4497",
+    "url": "/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "d2658f45c3018fbb7513",
+    "url": "/static/js/2.ef1740c6.chunk.js"
+  },
+  {
+    "revision": "9820deffb0aa42b3b25d5b59d7821436",
+    "url": "/static/js/2.ef1740c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59225e8bfbff6ec4497",
+    "url": "/static/js/main.ceb56fed.chunk.js"
+  },
+  {
+    "revision": "d991d79c90e5b142f6c7",
+    "url": "/static/js/runtime-main.7a5c7d53.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/static/media/bootstrap-icons.e07b538a.woff2"
+  }
+]);
