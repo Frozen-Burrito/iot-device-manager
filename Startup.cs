d4cd513250846df253223182558fd12badb49126
FrozenBurrito <f3rm3ndouza@outlook.com>
2021-10-01 06:50:04 -0500

    DB Migrations on Startup

--- Startup.cs
+++ Startup.cs
@@ -54,8 +54,10 @@ namespace DeviceRegistry
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext dataContext)
         {
+            dataContext.Database.Migrate();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
